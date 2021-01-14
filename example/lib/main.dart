9f00efcabf9e9a23c9a6e3b48d6126d701f37282
jitsm555 <jitsm555@gmail.com>
2021-01-14 21:30:23 +0530

    Added comments over examples.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -43,75 +43,75 @@ class _MyHomePageState extends State<MyHomePage> {
           child: SingleChildScrollView(
             child: Column(
               children: <Widget>[
-                // /// 1. Circular Image without border
-                // AvatarView(
-                //   radius: 60,
-                //   borderColor: Colors.yellow,
-                //   isOnlyText: false,
-                //   text: Text('C', style: TextStyle(color: Colors.white, fontSize: 50),),
-                //   avatarType: AvatarType.CIRCLE,
-                //   backgroundColor: Colors.red,
-                //   imagePath:
-                //   "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?cs=srgb&dl=pexels-pixabay-415829.jpg",
-                //   placeHolder: Container(
-                //     child: Icon(Icons.person, size: 50,),
-                //   ),
-                //   errorWidget: Container(
-                //     child: Icon(Icons.error, size: 50,),
-                //   ),
-                // ),
-                // SizedBox(height: 16,),
-                // /// 2. Circular Image with border
-                // AvatarView(
-                //   radius: 60,
-                //   borderWidth: 8,
-                //   borderColor: Colors.yellow,
-                //   isOnlyText: false,
-                //   text: Text('C', style: TextStyle(color: Colors.white, fontSize: 50),),
-                //   avatarType: AvatarType.CIRCLE,
-                //   backgroundColor: Colors.red,
-                //   imagePath:
-                //       "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?cs=srgb&dl=pexels-pixabay-415829.jpg",
-                //   placeHolder: Container(
-                //     child: Icon(Icons.person, size: 50,),
-                //   ),
-                //   errorWidget: Container(
-                //     child: Icon(Icons.error, size: 50,),
-                //   ),
-                // ),
-                // SizedBox(height: 16,),
-                // /// 3. Rectangle Image without border
-                // AvatarView(
-                //   radius: 60,
-                //   borderColor: Colors.grey,
-                //   avatarType: AvatarType.RECTANGLE,
-                //   backgroundColor: Colors.red,
-                //   imagePath:
-                //   "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg",
-                //   placeHolder: Container(
-                //     child: Icon(Icons.person, size: 50,),
-                //   ),
-                //   errorWidget: Container(
-                //     child: Icon(Icons.error, size: 50,),
-                //   ),
-                // ),
-                // SizedBox(height: 16,),
-                // /// 4. Rectangle Image with border
-                // AvatarView(
-                //   radius: 60,
-                //   borderWidth: 8,
-                //   borderColor: Colors.grey,
-                //   avatarType: AvatarType.RECTANGLE,
-                //   backgroundColor: Colors.red,
-                //   imagePath:
-                //   "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg",
-                //   placeHolder: Container(
-                //     child: Icon(Icons.person, size: 50,),
-                //   ),
-                //   errorWidget: Container(
-                //     child: Icon(Icons.error, size: 50,),
-                //   ),
-                // ),
+                /// 1. Circular Image without border
+                AvatarView(
+                  radius: 60,
+                  borderColor: Colors.yellow,
+                  isOnlyText: false,
+                  text: Text('C', style: TextStyle(color: Colors.white, fontSize: 50),),
+                  avatarType: AvatarType.CIRCLE,
+                  backgroundColor: Colors.red,
+                  imagePath:
+                  "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?cs=srgb&dl=pexels-pixabay-415829.jpg",
+                  placeHolder: Container(
+                    child: Icon(Icons.person, size: 50,),
+                  ),
+                  errorWidget: Container(
+                    child: Icon(Icons.error, size: 50,),
+                  ),
+                ),
+                SizedBox(height: 16,),
+                /// 2. Circular Image with border
+                AvatarView(
+                  radius: 60,
+                  borderWidth: 8,
+                  borderColor: Colors.yellow,
+                  isOnlyText: false,
+                  text: Text('C', style: TextStyle(color: Colors.white, fontSize: 50),),
+                  avatarType: AvatarType.CIRCLE,
+                  backgroundColor: Colors.red,
+                  imagePath:
+                      "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?cs=srgb&dl=pexels-pixabay-415829.jpg",
+                  placeHolder: Container(
+                    child: Icon(Icons.person, size: 50,),
+                  ),
+                  errorWidget: Container(
+                    child: Icon(Icons.error, size: 50,),
+                  ),
+                ),
+                SizedBox(height: 16,),
+                /// 3. Rectangle Image without border
+                AvatarView(
+                  radius: 60,
+                  borderColor: Colors.grey,
+                  avatarType: AvatarType.RECTANGLE,
+                  backgroundColor: Colors.red,
+                  imagePath:
+                  "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg",
+                  placeHolder: Container(
+                    child: Icon(Icons.person, size: 50,),
+                  ),
+                  errorWidget: Container(
+                    child: Icon(Icons.error, size: 50,),
+                  ),
+                ),
+                SizedBox(height: 16,),
+                /// 4. Rectangle Image with border
+                AvatarView(
+                  radius: 60,
+                  borderWidth: 8,
+                  borderColor: Colors.grey,
+                  avatarType: AvatarType.RECTANGLE,
+                  backgroundColor: Colors.red,
+                  imagePath:
+                  "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg",
+                  placeHolder: Container(
+                    child: Icon(Icons.person, size: 50,),
+                  ),
+                  errorWidget: Container(
+                    child: Icon(Icons.error, size: 50,),
+                  ),
+                ),
                 SizedBox(height: 16,),
                 /// 5. Text Avatar with circle
                 AvatarView(
@@ -147,7 +147,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
                 SizedBox(height: 16,),
-                /// 3. Wrong image URL
+                /// 7. Wrong image URL
                 AvatarView(
                   radius: 80,
                   borderColor: Colors.grey,
